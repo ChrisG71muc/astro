be13cfa97264694f923289a4d6c0aa61782cc6c7
ChrisG71muc <chrisg71muc@gmail.com>
2019-01-05 18:07:45 +0000

    adding file

---
+++ gps_info.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+# logger "script start"
+# sleep 60
+# logger "wakeup"
+# cd /home/pi
+# logger "calling the python script"
+# /usr/bin/python -u /home/pi/gps_info.py -d sh1106
+/usr/bin/python -u /home/pi/gps3_info.py -d sh1106
+# logger "gps_info done"
